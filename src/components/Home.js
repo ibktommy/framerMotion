00c4ccc84bb310ff69e8a1ff9cde1b56905c4bc7
ibktommy <atomquartz@gmail.com>
2022-10-07 10:59:12 +0100

    Remove animation from Home-header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
-      </Link>
-    </div>
-  )
-}
+	return (
+		<motion.div className="home container" animate={{}}>
+			<h2>Welcome to Pizza Joint</h2>
+			<Link to="/base">
+				<motion.button animate={{}}>Create Your Pizza</motion.button>
+			</Link>
+		</motion.div>
+	);
+};
 
 export default Home;
